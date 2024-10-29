fffe58d23d6d519fd49339b9839de72915eed37d
Maurine-cheptum <cheptummaurine12@gmail.com>
2024-10-29 13:26:55 +0300

    week 6

--- routes/appointments.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { bookAppointment } = require('../controllers/appointmentController');
-
-// Book an appointment
-router.post('/book', bookAppointment);
-
-module.exports = router;
